77a484091171267dbb8632a7d01c989fc5ccd681
Gage Hill <gage.hill@tlgcohort.com>
2023-11-15 11:37:32 -0700

    Added ShipType and Ship classes

---
+++ src/Ship.java
@@ -0,0 +1,11 @@
+class Ship {
+    private ShipType type;
+    private int hits;
+
+    public Ship(ShipType type) {
+        this.type = type;
+        this.hits = 0;
+    }
+
+    // TODO: call to firingBoard, shipBoard
+}
