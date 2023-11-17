edb6a98a26c4618364df5bdfe84e68555e928c48
Lance Evans <lance.evans@tlgcohort.com>
2023-11-17 16:24:45 -0700

    ShipBoardTest allShipsSunk()

---
+++ Test/com/battleship/player/ShipBoardTest.java
@@ -0,0 +1,17 @@
+/*
+package com.battleship.player;
+
+import com.battleship.boards.ShipBoard;
+
+import java.util.ArrayList;
+
+class ShipBoardTest {
+    public static void main(String[] args) {
+        ArrayList<String> ship = new ArrayList<>();
+        ship.add("a1");
+        ship.add("e2");
+        ship.add("d5");
+        ShipBoard.placeShip(ship);
+        System.out.println(ShipBoard.getShipBoard());
+    }
+}*/
