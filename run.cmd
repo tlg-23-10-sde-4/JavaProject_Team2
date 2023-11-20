481d150554c519c5e4afe7f7931d3335ffbf6635
Gage Hill <gage.hill@tlgcohort.com>
2023-11-20 11:39:31 -0700

    run.cmd file update

--- run.cmd
+++ run.cmd
@@ -1,3 +1,3 @@
    @echo off
    cls
-   java -classpath out\production\JavaProject_Team2;"lib\*" com.battleship.client
+   java -classpath out\production\blackjack;"lib\*" com.battleship.main.BattleshipGame
